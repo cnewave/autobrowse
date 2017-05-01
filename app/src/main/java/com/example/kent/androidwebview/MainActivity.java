313e4d6990ef82d6bac9eae9a78c4f56fe6e84a0
kent <kent@biotrump.com>
2017-05-01 17:11:48 +0800

    Refine the json and add editor menu

--- app/src/main/java/com/example/kent/androidwebview/MainActivity.java
+++ app/src/main/java/com/example/kent/androidwebview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kent.androidwebview;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.preference.PreferenceManager;
@@ -19,13 +20,19 @@ import android.widget.Button;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     final private String TAG = "AndroidWebView";
-    private ArrayList<String> mList = new ArrayList<>();
+    private List<String> mList = new ArrayList<>();
+    private List<WebInfo> mWebInfos = new ArrayList<>();
+
     private WebView mWebView = null;
     private int mMax = 10;
     private int SLEEP_INTERVAL = 30;
@@ -45,6 +52,10 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Log.d(TAG, "onResume");
         parseJson();
+        getConfig();
+    }
+
+    private void getConfig() {
 
         String t_Max = PreferenceManager.getDefaultSharedPreferences(this).getString("max_count", "10");
         String t_InterVal = PreferenceManager.getDefaultSharedPreferences(this).getString("sleep_interval", "30");
@@ -74,9 +85,13 @@ public class MainActivity extends AppCompatActivity {
     final private int START_AUTO = 0;
     final private int CONFIG = 1;
     final private int CHECK_MY_IP = 2;
+    final private int EDIT_LIST = 3;
+    final private int RESET_LIST = 4;
     final private String MENU_START = "start";
     final private String MENU_CONFIG = "config";
     final private String MENU_CHECKIP = "check ip";
+    final private String MENU_EDIT_LIST = "Edit list";
+    final private String MENU_RESET_LIST = "Reset list";
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -84,6 +99,8 @@ public class MainActivity extends AppCompatActivity {
         menu.add(0, START_AUTO, 0, MENU_START);
         menu.add(0, CONFIG, 1, MENU_CONFIG);
         menu.add(0, CHECK_MY_IP, 2, MENU_CHECKIP);
+        menu.add(0, EDIT_LIST, 3, MENU_EDIT_LIST);
+        menu.add(0, RESET_LIST, 4, MENU_RESET_LIST);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -127,10 +144,73 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         Log.d(TAG, "Check ip");
                         if (mWebView != null) {
-                            mWebView.loadUrl("https://www.whatismyip.com/");
+                            mWebView.loadUrl("https://whatismyip.li");
                         }
                     }
                 });
+            }
+            break;
+
+            case EDIT_LIST: {
+                Log.d(TAG, "launch alert dialog");
+
+                final int size = mWebInfos.size();
+                String[] items = new String[size];
+                final boolean[] enables = new boolean[size];
+
+                //for (WebInfo webIntem : mWebInfos) {
+                for (int i = 0; i < size; i++) {
+                    WebInfo webIntem = mWebInfos.get(i);
+                    String name = webIntem.getName();
+                    boolean enable = webIntem.isEnable();
+                    items[i] = name;
+                    enables[i] = enable;
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(" Edit Web List")
+                        .setMultiChoiceItems(items, enables, new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int indexSelected, boolean isChecked) {
+                                mWebInfos.get(indexSelected).setEnable(isChecked);
+                            }
+                        })
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                for (int i = 0; i < size; i++) {
+                                    Log.d(TAG, "Check list:" + i + " " + mWebInfos.get(i).isEnable());
+                                }
+                                // create json data
+                                String newConfig = createJson();
+                                // save to file
+                                saveToFile(newConfig);
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        }).show();
+            }
+            break;
+
+            case RESET_LIST: {
+                new AlertDialog.Builder(this)
+                        .setTitle("Delete File")
+                        .setMessage("Delete the file")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                removeFile();
+
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        }).show();
+
             }
             break;
             default:
@@ -139,9 +219,61 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    // File related
+    private final String fileName = "pixnet.json";
+
+    private void saveToFile(String data) {
+        // reparse json
+        Log.d(TAG, " saveToFile ");
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            outputStream.write(data.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG, " saveToFile done.");
+    }
+
+    private void removeFile() {
+        Log.d(TAG, " Reset list. ");
+        File file = new File(this.getFilesDir(), fileName);
+        if (file.exists()) {
+            Log.d(TAG, " Delete file list. ");
+            file.delete();
+        }
+        parseJson();
+    }
+
+    private String createJson() {
+        try {
+
+            Log.d(TAG, " create Json ");
+            JSONObject obj = new JSONObject();
+            JSONArray jlist = new JSONArray();
+            for (int i = 0; i < mWebInfos.size(); i++) {
+                WebInfo item = mWebInfos.get(i);
+                JSONObject jitem = new JSONObject();
+                jitem.put("name", item.getName());
+                jitem.put("enable", item.isEnable());
+                jitem.put("url", item.getURL());
+                jlist.put(jitem);
+            }
+            obj.put("web", jlist);
+            Log.d(TAG, "Create Json:" + obj.toString());
+            return obj.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
     private void parseJson() {
         try {
             mList.clear();
+            mWebInfos.clear();
             String config = loadConfig();
             //Log.d(TAG," parse Json "+config);
             JSONObject obj = new JSONObject(config);
@@ -151,7 +283,8 @@ public class MainActivity extends AppCompatActivity {
                 String name = item.getString("name");
                 String url = item.getString("url");
                 boolean enable = item.getBoolean("enable");
-
+                // Web info list
+                mWebInfos.add(new WebInfo(name, url, enable));
                 if (enable) {
                     Log.d(TAG, "Enable name:" + name + " enable:" + enable);
                     mList.add(url);
@@ -165,9 +298,18 @@ public class MainActivity extends AppCompatActivity {
 
     private String loadConfig() {
         Log.d(TAG, "loadConfig");
-        String json = null;
+        String json;
+        InputStream is = null;
         try {
-            InputStream is = getAssets().open("pixnet.json");
+
+            File file = new File(this.getFilesDir(), fileName);
+            if (file.exists()) {
+                Log.d(TAG, "Read file");
+                is = new FileInputStream(file);
+            } else {
+                Log.d(TAG, "load default config.");
+                is = getAssets().open(fileName);
+            }
             int size = is.available();
             byte[] buffer = new byte[size];
             is.read(buffer);
@@ -224,7 +366,7 @@ public class MainActivity extends AppCompatActivity {
                             public void run() {
                                 Log.d(TAG, "-->" + t_cURL);
                                 if (mWebView != null) {
-                                    // mWebView.loadUrl(t_cURL);
+                                    mWebView.loadUrl(t_cURL);
                                 }
                             }
                         });
@@ -251,4 +393,32 @@ public class MainActivity extends AppCompatActivity {
 
     private Thread mThread = null;
     private Object mWait = new Object();
+
+    private class WebInfo {
+        private boolean mEnalbe;
+        private String mURL;
+        private String mName;
+
+        public WebInfo(String name, String url, boolean enable) {
+            mName = name;
+            mURL = url;
+            mEnalbe = enable;
+        }
+
+        boolean isEnable() {
+            return mEnalbe;
+        }
+
+        String getName() {
+            return mName;
+        }
+
+        String getURL() {
+            return mURL;
+        }
+
+        void setEnable(boolean enable) {
+            mEnalbe = enable;
+        }
+    }
 }
